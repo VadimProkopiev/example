eab645f7e29f91e303336467fbe70fe93c3526b3
«Vadim <Hazz74@yandex.ru>
2022-10-27 23:50:45 +0500

    add comment

--- Program.cs
+++ Program.cs
@@ -3,11 +3,17 @@
 //4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
 //2 -> " -2, -1, 0, 1, 2"
 
-Console.WriteLine("Введите натуральное число ");
-int x=Convert.ToInt32 ( Console.ReadLine());
-int count = -x;
-while (count <= x)
-{
-   Console.Write(count+" ");
-   count = count +1;
-}
+// Console.WriteLine("Введите натуральное число ");
+// int x=Convert.ToInt32 ( Console.ReadLine());
+// int count = -x;
+// while (count <= x)
+// {
+//    Console.Write(count+" ");
+//    count = count +1;
+// }
+
+//Задача №7. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+//	456 -> 6
+//	782 -> 2
+//	918 -> 8
+
